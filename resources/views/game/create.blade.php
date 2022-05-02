cf112084237281088c00750f590900a83157bf06
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2022-05-02 06:35:58 -0500

    Cleaned up Game Submission Page

--- resources/views/game/create.blade.php
+++ resources/views/game/create.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="{{ route('game.store') }}" method="POST">
-        @csrf
-        <input type="datetime-local" name="start"/>
-        <input type="submit" value="Create Game"/>
+<div class="w-full max-w-xs">
+    <form method="POST" action="{{ route('game.store') }}" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      @csrf
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="start">
+          Start Time
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start" name="start" type="datetime-local">
+      </div>
+     
+      <div class="flex items-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Create Game
+        </button>
+      </div>
     </form>
+  </div>
 @endsection
