6b8a16b59d04113ee13e06fa8b36423da13b8d40
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2022-05-07 01:17:45 -0500

    Implemented Depost changes and ChipSchema

---
+++ app/Rules/CashSchema.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CashSchema implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $bundle)
+    {
+        return true;
+        $denominations =[
+            ".25",
+            "1",
+            "5",
+            "10",
+            "20",
+            "50",
+            "100"
+        ];
+
+        $final = [];
+
+        foreach ($denominations as $denomination) {
+            if (!array_key_exists($denomination, $bundle)) {
+                return false;
+            }
+
+            if ($bundle[$denomination] > 0) {
+                $final[$denomination] = $bundle[$denomination];
+            }
+        }
+
+        $chipsToHandOut = $bundle['chips-to-hand-out'];
+
+        $types = [
+            'color', 'denomination', 'amount'
+        ];
+        foreach ($bundle as $value) {
+            foreach ($types as $type) {
+                if (!array_key_exists($type, $value) || gettype($value[$type]) !== 'string') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
--- resources/views/game/deposit/create.blade.php
+++ resources/views/game/deposit/create.blade.php
@@ -4,21 +4,17 @@
 <div class="w-full max-w-xs">
     <form method="POST" action="{{ route('game.deposit.store', $game) }}" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         @csrf
-    <div class="mb-4">
-        {{ $playerSelect->render() }}
-        </div>
-        
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                Amount
-            </label>
-            <input name="amount" type="number" min="10" step="0.25" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+            {{ $playerSelect->render() }}
         </div>
 
+        @php($cashCounter = new App\View\Components\Input\CashCounter($game))
+        {{ $cashCounter->render() }}
+
         <div class="flex items-center">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Submit Deposit
-        </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                Submit Deposit
+            </button>
         </div>
     </form>
 </div>
