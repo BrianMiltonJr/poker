49e385b4b30c5aa61bf5fb94b773477e07ed25b3
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2022-05-07 02:34:14 -0500

    Fixed broken Datatable Import

--- resources/ts/bootstrap.ts
+++ resources/ts/bootstrap.ts
@@ -17,7 +17,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 window.$ = require('jquery');
 window.$.fn.select2 = require('select2');
-window.$.fn.DataTable = require('datatables');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
